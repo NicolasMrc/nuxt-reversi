f52c0513a8c60f010cb2aa1930972cce03ea8bdf
Nicolas Mercier <nyckoo@live.fr>
2019-05-23 16:53:28 +0200

    init board with first tiles

--- store/mutations.js
+++ store/mutations.js
@@ -1,12 +1,26 @@
 import * as disk from '../enums/diskState'
 import * as game from '../enums/gameState'
 
+function changeTile(state, x, y, color) {
+  state.board[x][y] = color
+}
+
 export default {
   initBoard(state) {
     state.board = Array(...Array(state.boardSize)).map(() =>
       Array(state.boardSize).fill(disk.NONE)
     )
-    // TODO init the 4 first disk
+    changeTile(state, state.boardSize / 2, state.boardSize / 2, disk.WHITE)
+    changeTile(
+      state,
+      state.boardSize / 2 - 1,
+      state.boardSize / 2 - 1,
+      disk.WHITE
+    )
+
+    changeTile(state, state.boardSize / 2 - 1, state.boardSize / 2, disk.BLACK)
+    changeTile(state, state.boardSize / 2, state.boardSize / 2 - 1, disk.BLACK)
+
     state.game = game.STARTED
   }
 }
