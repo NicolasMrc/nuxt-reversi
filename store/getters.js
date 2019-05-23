4ee31b9e6c008fdbc79e121edd99da64d5e6ec66
Nicolas Mercier <nyckoo@live.fr>
2019-05-23 16:58:15 +0200

    get players tiles positions

--- store/getters.js
+++ store/getters.js
@@ -1,8 +1,23 @@
+function playerTilesPositions(state, color) {
+  const playerTiles = []
+  for (let row = 0; row < state.boardSize; row++) {
+    for (let column = 0; row < state.boardSize; column++) {
+      if (state.board === color) {
+        playerTiles.push({ x: row, y: column })
+      }
+    }
+  }
+  return playerTiles
+}
+
 export default {
   board(state) {
     return state.board
   },
   boardSize(state) {
     return state.boardSize
+  },
+  possibleMoves(state, color) {
+    const playerTiles = playerTilesPositions(state, color)
   }
 }
